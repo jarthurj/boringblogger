a2ff49cf5a25706411f4c383ae25f29140a5233a
jarthurj <jaoldaker1@gmail.com>
2025-12-28 20:36:55 -0500

    working on authentication

--- blogs/blogapp/views.py
+++ blogs/blogapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request,'blogapp/login.html')
+
+@login_required
+def dashboard(request):
+    return render(request,"blogapp/dashboard.html")
+
