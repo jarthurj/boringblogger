245d614f9e69a6b2734a08df478509ccdf6b043b
jarthurj <jaoldaker1@gmail.com>
2025-12-31 15:43:40 -0500

    authentication done

--- blogs/accounts/views.py
+++ blogs/accounts/views.py
@@ -1,8 +1,4 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
-
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
@@ -11,15 +7,17 @@ from django.shortcuts import render, redirect
 def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
+        print(type(form))
+        print(form)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("home")
+            return redirect("/dashboard/")
     else:
         form = UserCreationForm()
     return render(request, "accounts/register.html", {"form": form})
 
-
+    
 def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
@@ -37,7 +35,5 @@ def logout_view(request):
     return redirect("login")
 
 
-@login_required
-def dashboard(request):
-    return render(request, "dashboard.html")
+
 
