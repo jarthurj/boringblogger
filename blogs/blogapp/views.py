3b46376e1d72ae0e4ee8ae279b1c60fd4e3dbbff
jarthurj <jaoldaker1@gmail.com>
2026-01-02 15:42:31 -0500

    added pagination and some tailwind

--- blogs/blogapp/views.py
+++ blogs/blogapp/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
+from django.core.paginator import Paginator
+
 @login_required
 def dashboard(request):
     posts = Post.objects.filter(author=request.user)
@@ -28,6 +30,16 @@ def newpost(request):
 
 def viewpost(request,pk):
     return render(request,"blogapp/viewpost.html",{"post":Post.objects.get(id=pk)})
+
+
+def postlist(request):
+    posts = Post.objects.all().order_by('-created_at')
+
+    paginator = Paginator(posts,1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,'blogapp/postlist.html',{'page_obj':page_obj})
+
 
 def home(request):
     return render(request, "blogapp/home.html", {"posts":Post.objects.all()})
